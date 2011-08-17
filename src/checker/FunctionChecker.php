de2d674db0ac6fba1801e937376cb018f82a79af
Mogria <m0gr14@gmail.com>
2011-08-17 17:05:46 +0200

    Improved FunctionChecker

--- src/checker/FunctionChecker.php
+++ src/checker/FunctionChecker.php
@@ -1,17 +1,23 @@
 <?php
 
 class FunctionChecker implements Checker {
-  protected $field;
+  protected $fields;
   protected $callback;
-  public function __construct($field, $callback) {
-    $this->field = $field;
+  public function __construct(Array $fields, $callback, Array $params, $field_param_num) {
+    $this->fields = $fields;
     $this->callback = $callback;
+    $this->field_param_num = $field_param_num;
   }
 
   public function check(Form $form) {
     $value = $form->getInputfieldByName($this->field);
-    if(is_callable($callback)) {
-      return $callback($value);
+    $params[$this->field_param_num] = $value;
+    $valid = true;
+    foreach($fields as $field) {
+      if(!user_func_call_array($callback, $params)) {
+        $valid = false;
+      }
     }
+    return $valid;
   }
 }
