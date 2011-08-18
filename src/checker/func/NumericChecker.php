f7aca3187b07dc312a8624f3ab6789e30cfa3d8a
Mogria <m0gr14@gmail.com>
2011-08-18 22:06:30 +0200

    Updated NumericChecker

--- src/checker/func/NumericChecker.php
+++ src/checker/func/NumericChecker.php
@@ -1,5 +1,7 @@
 <?php
 
-class NumericChecker extends AbstractRegularExpressionChecker {
-  protected $regular_expression = '/^[1-9][0-9]*[\.]{0,1}[0-9]$/i';
+class NumericChecker extends FunctionChecker {
+  public function __construct(Array $fields) {
+    parent::__construct($field, 'is_numeric', Array(), 1);
+  }
 }
