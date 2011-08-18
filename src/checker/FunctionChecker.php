e9395261f861fed48c0215ca73a2df3e53ae5f36
Mogria <m0gr14@gmail.com>
2011-08-18 07:21:32 +0200

    Improved FunctionChecker a bit

--- src/checker/FunctionChecker.php
+++ src/checker/FunctionChecker.php
@@ -3,7 +3,7 @@
 class FunctionChecker implements Checker {
   protected $fields;
   protected $callback;
-  public function __construct(Array $fields, $callback, Array $params, $field_param_num) {
+  public function __construct(Array $fields, $callback, Array $params = Array(), $field_param_num = 1) {
     $this->fields = $fields;
     $this->callback = $callback;
     $this->field_param_num = $field_param_num;
@@ -11,7 +11,7 @@ class FunctionChecker implements Checker {
 
   public function check(Form $form) {
     $value = $form->getInputfieldByName($this->field);
-    $params[$this->field_param_num] = $value;
+    $params[$this->field_param_num - 1] = $value;
     $valid = true;
     foreach($fields as $field) {
       if(!user_func_call_array($callback, $params)) {
