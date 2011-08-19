5cfa2c2d0b3bdd29d9375d9b8df3341bcca44e58
Mogria <m0gr14@gmail.com>
2011-08-19 17:26:06 +0200

    Created an abstract class out of the interface Checker, a few more changes needed...

--- src/checker/Checker.php
+++ src/checker/Checker.php
@@ -1,5 +1,34 @@
 <?php
 
-interface Checker {
-  public function check(Form $obj);
+abstract class Checker {
+
+  protected $errmsg = null;
+  protected $fields = Array();
+
+  public function getErrmsg() {
+    return $this->errmsg;
+  }
+
+  public function setErrmsg($value) {
+    $this->errmsg = $value;
+  }
+
+  public function validate() {
+    if(!$this->check() && ($msg = $this->getErrmsg()) !== null) {
+      foreach($this->fields as $field) {
+        $field->addErrormsg($msg);
+      }
+    }
+  }
+
+  abstract public function check(Form $obj);
+
+  public function __construct(Array $fields) {
+    foreach($fields as $field) {
+      if($field instanceof Inputfield) {
+        $this->fields = $fields;
+      }
+    }
+  }
+
 }
