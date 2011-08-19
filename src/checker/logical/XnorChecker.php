0c8146a55b9ddba3bf308c86d8d591443ecf0b44
Mogria <m0gr14@gmail.com>
2011-08-19 17:15:20 +0200

    fixed some issues with inheritance

--- src/checker/logical/XnorChecker.php
+++ src/checker/logical/XnorChecker.php
@@ -1,8 +1,8 @@
 <?php
 
-class XnorChecker {
+class XnorChecker extends AndChecker {
   public function __construct($checkers) {
-    $this->checker = new NotChecker(XnorChecker(count(func_get_args()) > 1 ? func_get_args() : $checkers[0]));
+    $this->checker = new NotChecker(XorChecker(count(func_get_args()) > 1 ? func_get_args() : $checkers[0]));
   }
 
   public function check(Form $form) {
