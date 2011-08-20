274f7fc6396dcde9869375ac206972672277186a
Mogria <m0gr14@gmail.com>
2011-08-20 21:03:47 +0200

    Improved the Abstract Checker class

--- src/checker/Checker.php
+++ src/checker/Checker.php
@@ -13,22 +13,38 @@ abstract class Checker {
     $this->errmsg = $value;
   }
 
-  public function validate() {
-    if(!$this->check() && ($msg = $this->getErrmsg()) !== null) {
-      foreach($this->fields as $field) {
-        $field->addErrormsg($msg);
-      }
-    }
+  protected function getValueOf(Form $form, $field) {
+    return $form->getInputfieldByName($file)->getValue();
   }
 
-  abstract public function check(Form $obj);
-
-  public function __construct(Array $fields) {
-    foreach($fields as $field) {
+  public function check(Form $form) {
+    //go through each
+    foreach($this->fields as $field) {
+      //If it is an Inputfield, get the value of it
       if($field instanceof Inputfield) {
-        $this->fields = $fields;
+        $value = $this->getValueOf($form Form);
+      } else {
+        $value = $field;
+      }
+      //check the value
+      if(($ret = $this->checkValue($value))) {
+        //Set Errmsg on failure
+        if(($msg = $this->getErrmsg()) != null) {
+          $this->addErrmsg($msg);
+        }
       }
     }
+    return $ret;
   }
 
+  abstract public function checkValue($value);
+
+  public function __construct($fields) {
+    //make $fields to an array and save it in the property $fields
+    if(is_array($fields)) {
+      $this->fields = $fields;
+    } else {
+      $this->fields = array($fields);
+    }
+  }
 }
