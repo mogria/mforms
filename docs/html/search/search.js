146e2635c410772f6f5e847aebbaa2f034930243
Mogria <m0gr14@gmail.com>
2011-08-21 16:29:25 +0200

    Updated Doxygen documentation

--- docs/html/search/search.js
+++ docs/html/search/search.js
@@ -263,7 +263,7 @@ function SearchBox(name, resultsPath, inFrame, label)
         }
         else
         {
-          node.innerHTML='&nbsp;';
+          node.innerHTML='&#160;';
         }
         j++;
       }
