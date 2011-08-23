98dd13a6d34c3141c7f65f446ad868c2a08cb7ce
Mogria <m0gr14@gmail.com>
2011-08-23 07:06:34 +0200

    Fixed List Checkers

--- src/checker/list/InListChecker.php
+++ src/checker/list/InListChecker.php
@@ -1,13 +1,13 @@
 <?php
 
-class InListChecker implements Checker {
+class InListChecker extends Checker {
 
   public function __construct($field, Array $list) {
-    partent::__construct($field);
+    parent::__construct($field);
     $this->list = $list;
   }
 
-  public function check(Form $form) {
-    return in_array($form->getInputfieldByName($this->field)->getValue(), $this->list);
+  public function checkValue($value) {
+    return in_array($field, $this->list);
   }
 }
