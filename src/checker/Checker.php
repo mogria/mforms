b094306f43660d7c25fbfdb25fe956f46406d845
Mogria <m0gr14@gmail.com>
2011-09-27 17:38:39 +0200

    Documented Checker & CheckerChain

--- src/checker/Checker.php
+++ src/checker/Checker.php
@@ -7,62 +7,110 @@ abstract class Checker {
 
   protected $autotrigger = true;
 
+  protected $form;
+
+  /**
+   * Get if errors should be triggered automaticly by calling the check-Method
+   *
+   * @return (bool) : true when autotriggered, false if not
+   */
   public function getAutotrigger() {
     return $this->autotrigger;
   }
 
+  /**
+   * Set if errors should be triggered automaticly by calling the check-Method
+   *
+   * @param (bool) $value 
+   */
   public function setAutotrigger($value) {
     $this->autotrigger = $value;
   }
 
-  protected $form;
-
+  /** 
+   *  Remove an added Inputfield
+   *
+   * @param (InputfieldInterface) $c : Inputfield/Value which should be removed from the list
+   */
   public function remove($c) {
     foreach($this->fields as $key => $member) {
       if($this->fields === $member) {
         unset($this->fields[$key]);
+        return;
       }
     }
   }
 
+  /** 
+   *  Add an Inputfield
+   *
+   * @param (mixed) $c : Inputfield/Value which should be added to the list
+   */
   public function add($c) {
      if($c instanceof InputfieldInterface) {
       $this->fields[] = $c;
     }
   }
 
+  /**
+   * Get the form to which this Checker is attached
+   *
+   * @return (Form) : The form
+   */
   public function getForm() {
     return $this->form;
   }
 
+  /**
+   * Set the form to which this Checker is attached
+   *
+   * @param (Form) $form : The form
+   */
   public function setForm($form) {
     $this->form = $form;
   }
 
+  /**
+   * Get the Error Message
+   *
+   * @return (string) : The Error Message
+   */
   public function getErrmsg() {
     return $this->errmsg;
   }
 
+  /**
+   * Set the Error Message
+   *
+   * @param (string) $value : The Error Message
+   */
   public function setErrmsg($value) {
     $this->errmsg = $value;
   }
 
+  /**
+   * Iterates each Check functions and Calls checkValue()
+   * Triggers an Error Message on the Inputfield on failure and if Autotrigger is set to TRUE
+   */
   public function check() {
     //go through each
     foreach($this->fields as $field) {
       //check the value
-
-      $value = ($f = $field instanceof InputfieldInterface ) ?
+      $value = ($field instanceof InputfieldInterface ) ?
         $field->getValue() :
         $field;
-      if (($ret = $this->checkValue($value)) && $this->getAutotrigger()) {
+      if (!($ret = $this->checkValue($value)) && $this->getAutotrigger() && $field instanceof InputfieldInterface) {
         //Set Errmsg on failure and autotrigger is turned on
         $this->triggerErrorMsg($field);
       }
     }
-    return $ret;
   }
 
+  /**
+   * triggers an error to the Inputfield
+   *
+   * @param (&InputfieldInterface) $input : The Error Message set will be attached to this Inputfield 
+   */
   public function triggerErrorMsg(InputfieldInterface &$input) {
     if(($msg = $this->getErrmsg()) != null) {
       $input = new ErrorMessage($input, $msg);
@@ -71,6 +119,11 @@ abstract class Checker {
 
   abstract public function checkValue($value);
 
+  /** Constructor
+   * Adds some Inputfields
+   *
+   * @param (mixed) $field: An Array or an Single Inputfield
+   */
   public function __construct($fields) {
     //make $fields to an array and save it in the property $fields
     if(is_array($fields)) {
--- src/checker/CheckerChain.php
+++ src/checker/CheckerChain.php
@@ -4,20 +4,43 @@ abstract class CheckerChain extends Checker {
 
   protected $loperation;
 
+  /**
+   * add a new Checker to the Chain
+   *
+   * @param (Checker) $c : An instance of Checker
+   */
   public function add($c) {
     if($c instanceof Checker) {
       $this->fields[] = $c;
     }
   }
 
+  /**
+   * Set the logical Operation which sould be used
+   * 
+   * @param (LogicalOperator) $l : An instance of LogicalOperator
+   */
   public function setLogicalOperation(LogicalOperator $l) {
     $this->loperation = $l;
   }
 
+  /**
+   * Get the logical Operator
+   *
+   * @return (LogicalOperator) : An instance of LogicalOperator
+   */
   public function getLogicalOperation() {
     return $this->loperation;
   }
 
+  /** Constructor
+   * Calls add() on each given Checker
+   * Sets the LogicalOperator
+   * If nothing given an AndOperator is used
+   *
+   * @param (Array) $checkers : Instances of Checker in an Array
+   * @param (LogicalOperator) $operation : An Logical Opeator: default AndOpetator
+   */
   public function __construct($checkers, $operation = null) {
     parent::__construct($checkers);
     if($operation === null) {
@@ -26,6 +49,11 @@ abstract class CheckerChain extends Checker {
     $this->setOperation($operation);
   }
 
+  /**
+   * calls the check Method of each added Checker and Combines it's solution with the set LogicalOperator
+   *
+   * @return (bool) 
+   */
   public function check() {
     $anz = count($this->fields = array_keys($this->fields));
     
