ef011ae927614e852fab7062e902e3b4a109c6f0
Mogria <m0gr14@gmail.com>
2011-10-04 06:53:24 +0200

    Removed unneeded functions & checked @todo note

--- src/decorators/Decorator.php
+++ src/decorators/Decorator.php
@@ -27,23 +27,7 @@ abstract class Decorator {
     }
   }
 
-  /** @todo: contains the second parameter the params?  */
   public function __call($method, $params) {
     return call_user_func_array(array($this->object, $method), $params);
   }
-  /** public function __get($key) {
-    return $this->object->{$key};
-  }
-
-  public function __set($key, $value) {
-    return $this->object->{$key} = $value;
-  }
-
-  public function __isset($key) {
-    return isset($this->object->{$key});
-  }
-
-  public function __unset($key) {
-    unset($this->object->{$key});
-  } */
 }
