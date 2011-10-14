639524aebd20c5357e82f7756ec86f032abdf713
Mogria <m0gr14@gmail.com>
2011-10-14 17:59:42 +0200

    started to implement an upload_to Method for Filechooser

--- src/input/Filechooser.php
+++ src/input/Filechooser.php
@@ -11,5 +11,24 @@ class Filechooser extends Inputfield {
   public function getType() {
     return "file";
   }
+
+  /**
+   * if a file has been uploaded, so you can call
+   * this function to validate that and move the file
+   * to the correct location
+   *
+   * @param (string) $location_filename : the new location of the file
+   * @param (int) $max_size : the maximal size of the uploaded file in bytes, false -> unlimited
+   * @param (Array) $mine_types : An array containing the allowed/disallowd MIME-TYPE of the file
+   * @param (bool) $is_whitelist : is the list of mime_types a while-(true) or a blacklist(false)
+   */
+  public function upload_to($location_filename, $max_size = false, $mime_types = Array(), $is_whitelist = false) {
+    $file = $this->getValue();
+    if(isset($file['tmp_name'], $file['name'], $file['size'])) {
+      /** @todo the upload function finish, by using checkers */
+    } else {
+      throw new BadMethodCallException("there is no uploaded file in value property!");
+    }
+  }
 }
 
